54dc3e0ae5a5bcf9fd8f18582c7ee5ff4405ef68
Munerin22 <sg.7scy@gmail.com>
2020-12-07 23:17:33 +0900

    4?回目のコミット

--- practice_for.php
+++ practice_for.php
@@ -5,14 +5,14 @@
 <title>繰り返し表示</title>
 </head>
 <body>
-<h1><font size="3">1～70までを表示します(3の倍数で"git"を付け加える)</font></h1>
+<h1><font size="3">1～80までを表示します(3の倍数で"git"を付け加える)</font></h1>
 <?php
 //forでカウント
-for ($count = 1; $count <= 70; $count++) {
+for ($count = 1; $count <= 80; $count++) {
 	//カウントを出力
 	echo $count;
 
-	//3の倍数→7で割り切れる→余り'0'
+	//7の倍数→7で割り切れる→余り'0'
 	//余りが'0'かどうかを判別して出力
 	if ($count % 7 == 0) {
 		echo ':git';
